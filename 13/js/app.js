f1017946a131a97b41a8073bdd8a5056ab00ba0e
Vladislav Kopylash <vladik.kopilash@gmail.com>
2015-07-23 18:06:25 +0300

    fised some issues in 13 homework

---
+++ 13/js/app.js
@@ -0,0 +1,8 @@
+var menuToggle = function() {
+
+	if (document.getElementById("menu").style.display!='block') {
+		document.getElementById("menu").style.display="block";	
+	} else {
+		document.getElementById("menu").style.display="none";
+	}
+}
